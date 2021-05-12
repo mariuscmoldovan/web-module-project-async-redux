dded5662f40bbc920fe140df4afa644a68e465ff
mariusmoldovan <biris_21@yahoo.com>
2021-05-12 18:55:20 -0500

    first commit

---
+++ spacecraft/src/components/Spacecraft.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Spacecraft = () => {
+    return (
+        <div className="spacecraft-div">
+            <h1>SPACECRAFT</h1>
+        </div>
+    )
+}
+export default Spacecraft
